31f8cdc0dd2314c827fea8efaee10211f3256ec1
laurievale <laurievalediego@gmail.com>
2021-04-27 19:53:08 -0600

    Mas json

--- frecuencia.php
+++ frecuencia.php
@@ -34,16 +34,34 @@
     $result9 = mysqli_num_rows($query9);
     $query10 = mysqli_query($db, "SELECT * FROM respuestas WHERE respuesta = 'noPlataforma' and id_pregunta = 14");
     $result10 = mysqli_num_rows($query10);
+
+    //cantidad de usuarios registrados y encuestas llenas
     $query11 = mysqli_query($db, "SELECT * FROM users");
     $result11 = mysqli_num_rows($query11);
+    $query12 = mysqli_query($db, "SELECT DISTINCT users_id FROM respuestas");
+    $result12 = mysqli_num_rows($query12);
+    $result13 = $result11 - $result12;
 
+
+    //frecuencia de compra antes y ahora
+    $query14 = mysqli_query($db, "SELECT * FROM respuestas where respuesta = '1a5'and id_pregunta = 'freq20'");
+    $result14 = mysqli_num_rows($query14);
+    $query15 = mysqli_query($db, "SELECT * FROM respuestas where respuesta = '6a10'and id_pregunta = 'freq20'");
+    $result15 = mysqli_num_rows($query15);
+    $query16 = mysqli_query($db, "SELECT * FROM respuestas where respuesta = '+10'and id_pregunta = 'freq20'");
+    $result16 = mysqli_num_rows($query16);
+    $query17 = mysqli_query($db, "SELECT * FROM respuestas where respuesta = '1enMeses'and id_pregunta = 'freq20'");
+    $result17 = mysqli_num_rows($query17);
+    $query18 = mysqli_query($db, "SELECT * FROM respuestas where respuesta = 'noFrecuencia'and id_pregunta = 'freq20'");
+    $result18 = mysqli_num_rows($query18);
 
 
 
 
 
     $stats = array("linea" => $result1, "fisico" => $result2, "amazon" => $result3, "mercado"=>$result4, "fb"=>$result5,
-     "alibaba"=>$result6, "ebay"=>$result7, "eshop"=>$result8, "otros"=>$result9, "noPlataforma"=>$result10, "totalSample"=>$result11, "answered"=>30, "notAnswered"=>9);
+     "alibaba"=>$result6, "ebay"=>$result7, "eshop"=>$result8, "otros"=>$result9, "noPlataforma"=>$result10, 
+     "totalSample"=>$result11, "answered"=>$result12, "notAnswered"=>$result13);
     
 
 
