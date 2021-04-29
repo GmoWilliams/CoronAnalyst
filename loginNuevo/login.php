a099f665c910978ace9a80682bafa4e93fe4f552
GmoWilliams <guillermowilliams01@gmail.com>
2021-04-29 09:35:10 -0700

    Login Improved

--- loginNuevo/login.php
+++ loginNuevo/login.php
@@ -9,13 +9,15 @@ if(isset($_POST['login-btn'])) {
 
     $user = $_POST['user-name'];
     $password = $_POST['user-pass'];
+    //$hashed_password = $row['password'];
+    $hashed_password ='';
 
     try {
       $SQLQuery = "SELECT * FROM users WHERE username = :username";
       $statement = $conn->prepare($SQLQuery);
       $statement->execute(array(':username' => $user));
 
-      while($row = $statement->fetch()) {
+      if($row = $statement->fetch()) {
         $id = $row['id'];
         $hashed_password = $row['password'];
         $username = $row['username'];
@@ -27,24 +29,38 @@ if(isset($_POST['login-btn'])) {
           //header('location: index.html');
 
           $query = mysqli_query($db, "SELECT * FROM respuestas WHERE users_id = '$id' ");
-          if (mysqli_num_rows($query) > 0){
+          if (mysqli_num_rows($query) > 0) {
           header('location: ../dashboard.php');
           }
 
           else {
           header('location: ../index.php');
           }
-
         }
+          
         else {
 
-          //echo "Error: Nombre de usuario o contrasenia invalida. Por favor regrese y vuelva a ingresar las credenciales";
-          echo "<script>
-          alert('Error: Nombre de usuario o contrasenia invalida. Por favor regrese y vuelva a ingresar las credenciales');
+        
+        echo "<script>
+          alert('Error: Contraseña inválida. Por favor regrese y verifique que la ha escrito correctamente');
           window.location.href='../welcome.html';
           </script>";
+          
+          
+          
+  
         }
       }
+
+      else {
+        echo "<script>
+          alert('Error: Este usuario no existe. Por favor regrese y vuelva a ingresar las credenciales');
+          window.location.href='../welcome.html';
+          </script>";
+      }
+      
+      
+      
     }
     catch (PDOException $e) {
       echo "Error: " . $e->getMessage();
